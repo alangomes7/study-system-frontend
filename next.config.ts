fc6d4b73c34fb163916bdd06848bfbedac2b33a9
Alan Gomes <alan.gomestemail@gmail.com>
2025-11-04 18:15:05 -0300

    env server configurations

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
 import type { NextConfig } from 'next';
+import dotenv from 'dotenv';
+import path from 'path';
 
-const IP_SERVER = process.env.IP_SERVER || '192.168.0.177';
-const PORT = process.env.PORT || '8080';
+dotenv.config({ path: path.resolve(process.cwd(), '.env.local') });
 
-process.env.NEXT_PUBLIC_SPRINGBOOT_SERVER_IP_ADDRESS = `http://${IP_SERVER}:${PORT}`;
+const IP_SERVER = process.env.SPRINGBOOT_PORT_IP_SERVER || 'localhost';
+const PORT = process.env.SPRINGBOOT_PORT || '8080';
+
+const SPRINGBOOT_URL = `http://${IP_SERVER}:${PORT}`;
+
+process.env.NEXT_PUBLIC_SPRINGBOOT_SERVER_IP_ADDRESS = SPRINGBOOT_URL;
+
+console.log('-------------------------------------------');
+console.log(`Loaded .env.local configuration:`);
+console.log(`Server: ${IP_SERVER}`);
+console.log(`Port: ${PORT}`);
+console.log(`Public API URL: ${SPRINGBOOT_URL}`);
+console.log('-------------------------------------------');
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
